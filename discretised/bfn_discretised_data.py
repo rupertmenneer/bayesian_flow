dd432b929daef2526e563f8d5c1ada04a596db06
Rupert Menneer <rupertmenneer@gmail.com>
2024-02-24 19:11:11 +0000

    reverted to older quantize method (ensured correctness)

--- discretised/bfn_discretised_data.py
+++ discretised/bfn_discretised_data.py
@@ -64,15 +64,17 @@ class DiscretisedBimodalData(Dataset):
         normalized_data = (bimodal_data - min_val) / (max_val - min_val) * 2 - 1
         
         # discretise the bimodal distribution into K bins
-        discretised_data = quantize_tensor(normalized_data, self.k_upper, self.k_lower, self.k_centers)
+        discretised_data = quantize(normalized_data, self.k)
 
         return discretised_data
+    
+def idx_to_float(idx: np.ndarray, num_bins: int):
+    flt_zero_one = (idx + 0.5) / num_bins
+    return (2.0 * flt_zero_one) - 1.0
 
+def float_to_idx(flt: np.ndarray, num_bins: int):
+    flt_zero_one = (flt / 2.0) + 0.5
+    return torch.clamp(torch.floor(flt_zero_one * num_bins), min=0, max=num_bins - 1).long()
 
-def quantize_tensor(data, upper, lower, center):
-    new_data = torch.zeros_like(data)
-    for idx in range(len(data)):
-        for k in range(len(upper)):
-            if data[idx] >= lower[k] and data[idx] < upper[k]:
-                new_data[idx] = center[k]
-    return new_data
+def quantize(flt, num_bins: int):
+    return idx_to_float(float_to_idx(flt, num_bins), num_bins)
