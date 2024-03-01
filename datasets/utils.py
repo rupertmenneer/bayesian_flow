4441a890c21560095f9fa430699c19f338211558
Rupert Menneer <rupertmenneer@gmail.com>
2024-03-01 10:55:35 +0000

    added image grid method

--- datasets/utils.py
+++ datasets/utils.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 from torch import Tensor
-
+from torchvision.utils import make_grid
 
 def idx_to_float(idx: np.ndarray, num_bins: int):
     flt_zero_one = (idx + 0.5) / num_bins
@@ -15,7 +15,9 @@ def float_to_idx(flt: np.ndarray, num_bins: int):
 def quantize(flt, num_bins: int):
     return idx_to_float(float_to_idx(flt, num_bins), num_bins)
 
-    
+def get_image_grid_from_tensor(image_tensor, nrows=1):
+    return make_grid(image_tensor, nrow=nrows, normalize=True)
+
 def plot_tensor_images(images, n=8):
     fig, axes = plt.subplots(1, n, figsize=(10, 10))
     for i in range(n):
